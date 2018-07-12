a65054e29e0900172dfd2f986563a415a4ff2fcb
Wolf Clément <c.wolf@kiwec.net>
2018-07-12 12:45:41 +0200

    Fix socket event callbacks not registering more than once

--- EventLoop.js
+++ EventLoop.js
@@ -1,41 +1,36 @@
 class EventLoop {
 	constructor() {
-		this.eventList = [];
-		this.sockEvents = ['alert', 'ban', 'clear', 'connect', 'connected_list', 
-		'disconnect', 'error', 'forcepost', 'forcereload', 'info',
-		'message', 'message_delete', 'play_sound', 'pseudo_info',
-		'room_update', 'room_info', 'yt_room_history', 'yt_room_waitlist',
-		'yt_sync', 'yt_search', 'mouse_position', 'mouse_destroy',
-		'room_list', 'success', 'typing_list'];
+		this.callbacks = {};
 	}
 
 	// Fires an event
 	fire(name, args) {
-		if(typeof this.eventList[name] === 'undefined') return;
-		for (const i in this.eventList[name]) {
-			this.eventList[name][i](args);
+		if(typeof this.callbacks[name] === 'undefined') return;
+		for(let callback of this.callbacks[name]) {
+			callback(args);
 		}
 	}
 
 	// Registers socket.io events
 	initSock(sock) {
-		function eventCaller(args) {
-			this.fire(this.name, args);
-		}
-		for (const i in this.sockEvents) {
-			sock.on(this.sockEvents[i], eventCaller.bind({
-				fire: this.fire.bind(this),
-				name: this.sockEvents[i]
-			}));
+		let sockEvents = ['alert', 'ban', 'clear', 'connect',
+		'connected_list', 'disconnect', 'error', 'forcepost', 'forcereload',
+		'info', 'message', 'message_delete', 'play_sound', 'pseudo_info',
+		'room_update', 'room_info', 'yt_room_history', 'yt_room_waitlist',
+		'yt_sync', 'yt_search', 'mouse_position', 'mouse_destroy',
+		'room_list', 'success', 'typing_list'];
+
+		for (let eventName of sockEvents) {
+			sock.on(eventName, data => this.fire(eventName, data));
 		}
 	}
 
 	// Registers an event
 	on(name, callback) {
-		if(typeof this.sockEvents[name] === 'undefined') {
-			this.eventList[name] = [];
+		if(typeof this.callbacks[name] === 'undefined') {
+			this.callbacks[name] = [];
 		}
-		this.eventList[name].push(callback);
+		this.callbacks[name].push(callback);
 	}
 }
 
